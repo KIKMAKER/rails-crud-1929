d79daec5237c30ccb45a1a8c69e2218078e21fe1
Kiki Kennedy <kristen.c.kennedy@gmail.com>
2025-02-25 09:37:15 +0200

    added address to restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_25_071736) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_25_073300) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
